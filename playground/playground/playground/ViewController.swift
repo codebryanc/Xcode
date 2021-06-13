734985497217d0ded1f9b1d4b40c808d1c37c78a
Bryan Alejandro Cubillos Prieto <bryan.cubillos@bizagi.com>
2021-06-12 22:48:30 -0500

    Add new things / related with center layout / constrain

---
+++ playground/playground/playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by Bryan Alejandro Cubillos Prieto on 12/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
